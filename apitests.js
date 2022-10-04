1269e4df5507ee22ec21673ae37fafc20d21a021
DoozyKiddo <ryanoyan@gmail.com>
2022-10-03 20:03:45 -0700

    RyanCommit

--- apitests.js
+++ apitests.js
@@ -32,3 +32,32 @@ function robAPI(link) {
     return(err);
     });
 }
+
+function ryanAPI(link) {
+    fetch(link)
+    .then(
+    // HTTP GET Request: 
+    function(response) {
+        // Error handling
+        if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+        return;
+        }
+
+        // Examine the text in the response
+        response.json().then(function(data) {
+        console.log(data); // Use console to check data object contents
+
+        // Popup Notification:
+        tata.log("Random question: ", data,{
+            position: 'tm',
+          });
+        });
+    }
+    )
+    // Error Handling
+    .catch(function(err) {
+    return(err);
+    });
+}
