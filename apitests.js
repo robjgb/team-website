df91c1673c35d2628e910c0ace5b96306d822389
Theo <justrandomess@gmail.com>
2022-10-03 22:03:46 -0700

    HTTP API Commit

--- apitests.js
+++ apitests.js
@@ -90,3 +90,32 @@ function aaronAPI(link) {
     return(err);
     });
 }
+
+function theoAPI(link) {
+    fetch(link)
+    .then(
+    // HTTP GET Request: 
+    function(response) {
+        // Error handling
+        if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+        return;
+        }
+
+        // Examine the text in the response
+        response.json().then(function(data) {
+        console.log(data); // Use console to check data object contents
+
+        // Popup Notification:
+        tata.log("Overwatch Hero Generator", data,{
+            position: 'tm',
+          });
+        });
+    }
+    )
+    // Error Handling
+    .catch(function(err) {
+    return(err);
+    });
+}
